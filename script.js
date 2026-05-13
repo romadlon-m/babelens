e41f3cd245ff888d608ad838420d888a42f7d057
M M Romadlon <akhulromadlon@gmail.com>
2026-05-13 09:12:59 +0700

    Initial script.js

--- script.js
+++ script.js
@@ -1,1 +1,118 @@
+const API_URL =
+  "https://script.google.com/macros/s/AKfycbwCCgzR89n5wLrSRkWqCCZDLAvzbWwx9Y-3Dhn5QpHf2NSXFhoKdjkJmN_wG-EZKqu6/exec";
 
+
+// =====================================
+// SEARCH
+// =====================================
+function search(page = 1) {
+
+  const keyword =
+    document
+      .getElementById("keyword")
+      .value;
+
+  const params = new URLSearchParams({
+    action: "searchNews",
+
+    keyword,
+
+    page,
+
+    pageSize: 25,
+
+    f_title: true,
+    f_summary: true,
+    f_full: false,
+
+    pdrb_relevan: true
+  });
+
+  document.getElementById("result")
+    .innerHTML = "Loading...";
+
+  fetch(`${API_URL}?${params}`)
+    .then(res => res.json())
+    .then(render)
+    .catch(err => {
+
+      console.error(err);
+
+      document.getElementById("result")
+        .innerHTML =
+          "Failed to load data";
+    });
+}
+
+
+// =====================================
+// RENDER
+// =====================================
+function render(data) {
+
+  document.getElementById("meta")
+    .innerHTML =
+      `Found ${data.totalFound} articles`;
+
+  if (!data.rows.length) {
+
+    document.getElementById("result")
+      .innerHTML =
+        "No results";
+
+    return;
+  }
+
+  let html = `
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Title</th>
+        <th>Summary</th>
+        <th>Source</th>
+      </tr>
+  `;
+
+  data.rows.forEach(r => {
+
+    html += `
+      <tr>
+
+        <td>
+          ${r.publication_datetime}
+        </td>
+
+        <td>
+          <a
+            href="${r.url}"
+            target="_blank"
+          >
+            ${r.title}
+          </a>
+        </td>
+
+        <td class="summary">
+          ${r.summary}
+        </td>
+
+        <td>
+          ${r.source}
+        </td>
+
+      </tr>
+    `;
+  });
+
+  html += "</table>";
+
+  document.getElementById("result")
+    .innerHTML = html;
+}
+
+
+// =====================================
+// FIRST LOAD
+// =====================================
+window.onload = () => {
+  search(1);
+};
